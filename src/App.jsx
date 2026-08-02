401592f143d6f134182411b0802c01e290b974e2
IsaiasRuizT <isaiasruizt@gmail.com>
2026-08-01 23:47:50 -0400

    App.jsx basic design made it

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,9 @@
+export default function App() {
+  return (
+    <div className="landing">
+      <h1>Hunter Journal</h1>
+      <p>A companion app for Monster Hunter World hunters.</p>
+    </div>
+  );
+}
 
